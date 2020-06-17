f13752731f07ea8d9dc9f959c7320934b78bbfa0
Keith Frost <57332657+krfrost1@users.noreply.github.com>
2020-06-16 17:01:53 -0700

    extra plus needed

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    //extra plus needed to coerce variable
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
